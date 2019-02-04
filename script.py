c40587963fb0890fbdd154394e6b78b40cdddef1
kris <gggf98@gmail.com>
2019-02-04 13:18:07 +0300

    add script for get 10 words and learn everyday. marked first 10 words in xls.

---
+++ script.py
@@ -0,0 +1,18 @@
+import random
+
+
+words = []
+with open("text") as f:
+    for line in f:
+        words.append(line)
+
+print (random.choice(words))
+print (random.choice(words))
+print (random.choice(words))
+print (random.choice(words))
+print (random.choice(words))
+print (random.choice(words))
+print (random.choice(words))
+print (random.choice(words))
+print (random.choice(words))
+print (random.choice(words))
